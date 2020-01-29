e76dfdefa87f6639db281b4c5c2808f50810025a
Thabiso Pheko <tabs150@gmail.com>
2020-01-29 10:23:39 +0200

    Added native html link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 
 const IndexPage = () => {
-  return ( 
+  return (
     <div>
       <h1>Hello.</h1>
       <h2>I'm Thabiso, a learner developer from the Vaal</h2>
+      <p>
+        Heed a developer? <a href="/contact">Contact me</a>
+      </p>
     </div>
-   );
+  )
 }
- 
-export default IndexPage;
+
+export default IndexPage
